7ab91db2f235af11b6c1b4fe5187c5017d59f1e9
abc <abc@163.com>
2026-06-21 19:46:56 +0800

    配置 GitHub Pages 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/shuyiwangye/',
   server: {
     port: 3000,
     host: '0.0.0.0',
